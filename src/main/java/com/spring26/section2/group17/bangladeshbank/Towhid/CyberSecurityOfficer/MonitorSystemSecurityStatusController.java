01393d0cd7633005331dfa5fdd40a2471294b5e2
Towhid Azad <2312142@iub.edu.bd>
2026-03-31 03:54:43 +0600

    inital commit

--- src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/CyberSecurityOfficer/MonitorSystemSecurityStatusController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/CyberSecurityOfficer/MonitorSystemSecurityStatusController.java
@@ -1,7 +1,28 @@
 package com.spring26.section2.group17.bangladeshbank.Towhid.CyberSecurityOfficer;
 
+import javafx.scene.control.*;
+
 public class MonitorSystemSecurityStatusController
 {
+    @javafx.fxml.FXML
+    private ComboBox customRangeBox;
+    @javafx.fxml.FXML
+    private RadioButton weekRadio;
+    @javafx.fxml.FXML
+    private RadioButton monthRadio;
+    @javafx.fxml.FXML
+    private TextField threatLevelField;
+    @javafx.fxml.FXML
+    private TextField alertsField;
+    @javafx.fxml.FXML
+    private RadioButton todayRadio;
+    @javafx.fxml.FXML
+    private Button updateButton;
+    @javafx.fxml.FXML
+    private TextArea dashboardArea;
+    @javafx.fxml.FXML
+    private ComboBox healthComboBox;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
