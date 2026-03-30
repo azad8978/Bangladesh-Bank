465eb14784606442e55191af5c72ac787389ddaf
Towhid Azad <2312142@iub.edu.bd>
2026-03-31 03:23:45 +0600

    inital commit

--- src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/CyberSecurityOfficer/InvestigateSuspiciousActivitiesController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/CyberSecurityOfficer/InvestigateSuspiciousActivitiesController.java
@@ -1,7 +1,36 @@
 package com.spring26.section2.group17.bangladeshbank.Towhid.CyberSecurityOfficer;
 
+import javafx.scene.control.*;
+
 public class InvestigateSuspiciousActivitiesController
 {
+    @javafx.fxml.FXML
+    private TableColumn actionColumn;
+    @javafx.fxml.FXML
+    private TextArea notesArea;
+    @javafx.fxml.FXML
+    private RadioButton highRadio;
+    @javafx.fxml.FXML
+    private TableView logsTable;
+    @javafx.fxml.FXML
+    private RadioButton lowRadio;
+    @javafx.fxml.FXML
+    private ComboBox categoryComboBox;
+    @javafx.fxml.FXML
+    private Button investigateButton;
+    @javafx.fxml.FXML
+    private PasswordField passwordField;
+    @javafx.fxml.FXML
+    private RadioButton mediumRadio;
+    @javafx.fxml.FXML
+    private Button saveButton;
+    @javafx.fxml.FXML
+    private TextField investigatorField;
+    @javafx.fxml.FXML
+    private TableColumn sourceColumn;
+    @javafx.fxml.FXML
+    private TableColumn timestampColumn;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
--- src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/CyberSecurityOfficer/ManageUserAccessViolationsController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/CyberSecurityOfficer/ManageUserAccessViolationsController.java
@@ -1,7 +1,36 @@
 package com.spring26.section2.group17.bangladeshbank.Towhid.CyberSecurityOfficer;
 
+import javafx.scene.control.*;
+
 public class ManageUserAccessViolationsController
 {
+    @javafx.fxml.FXML
+    private ComboBox durationComboBox;
+    @javafx.fxml.FXML
+    private TextArea notesArea;
+    @javafx.fxml.FXML
+    private TableView violationTable;
+    @javafx.fxml.FXML
+    private Button applyButton;
+    @javafx.fxml.FXML
+    private Button clearButton;
+    @javafx.fxml.FXML
+    private TableColumn userIdColumn;
+    @javafx.fxml.FXML
+    private TextField userIdField;
+    @javafx.fxml.FXML
+    private RadioButton permanentRadio;
+    @javafx.fxml.FXML
+    private Button restrictButton;
+    @javafx.fxml.FXML
+    private PasswordField passwordField;
+    @javafx.fxml.FXML
+    private TableColumn dateColumn;
+    @javafx.fxml.FXML
+    private TableColumn violationTypeColumn;
+    @javafx.fxml.FXML
+    private RadioButton temporaryRadio;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
