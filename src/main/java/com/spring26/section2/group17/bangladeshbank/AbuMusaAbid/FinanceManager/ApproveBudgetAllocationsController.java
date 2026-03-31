332cc5e829a619f74c23134ec939fc1823ae2d7f
abumusaabid <2310141@iub.edu.bd>
2026-03-31 22:11:36 +0600

    intial commit

--- src/main/java/com/spring26/section2/group17/bangladeshbank/AbuMusaAbid/FinanceManager/ApproveBudgetAllocationsController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/AbuMusaAbid/FinanceManager/ApproveBudgetAllocationsController.java
@@ -1,7 +1,28 @@
 package com.spring26.section2.group17.bangladeshbank.AbuMusaAbid.FinanceManager;
 
+import javafx.scene.control.*;
+
 public class ApproveBudgetAllocationsController
 {
+    @javafx.fxml.FXML
+    private TableColumn amountColumn;
+    @javafx.fxml.FXML
+    private TableColumn departmentColumn;
+    @javafx.fxml.FXML
+    private TableColumn requestedByColumn;
+    @javafx.fxml.FXML
+    private TableColumn purposeColumn;
+    @javafx.fxml.FXML
+    private Button approveButton;
+    @javafx.fxml.FXML
+    private Button rejectButton;
+    @javafx.fxml.FXML
+    private TextField commentField;
+    @javafx.fxml.FXML
+    private TextArea detailsArea;
+    @javafx.fxml.FXML
+    private TableView budgetTable;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
--- src/main/java/com/spring26/section2/group17/bangladeshbank/AbuMusaAbid/SystemUser/ViewSimulationDashboardsController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/AbuMusaAbid/SystemUser/ViewSimulationDashboardsController.java
@@ -1,7 +1,26 @@
 package com.spring26.section2.group17.bangladeshbank.AbuMusaAbid.SystemUser;
 
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+
 public class ViewSimulationDashboardsController
 {
+    @javafx.fxml.FXML
+    private Button applyButton;
+    @javafx.fxml.FXML
+    private TableView simulationTable;
+    @javafx.fxml.FXML
+    private BarChart barChart;
+    @javafx.fxml.FXML
+    private ComboBox timePeriodCombo;
+    @javafx.fxml.FXML
+    private ComboBox scenarioCombo;
+    @javafx.fxml.FXML
+    private LineChart lineChart;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
