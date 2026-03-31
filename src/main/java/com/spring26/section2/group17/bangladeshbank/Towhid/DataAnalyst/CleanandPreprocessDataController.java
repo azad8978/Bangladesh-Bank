116bf3606b0be2dd68b8d57231862cc14b1c8133
Towhid Azad <2312142@iub.edu.bd>
2026-03-31 19:01:23 +0600

    inital commit

--- src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/DataAnalyst/CleanandPreprocessDataController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/DataAnalyst/CleanandPreprocessDataController.java
@@ -3,5 +3,28 @@ package com.spring26.section2.group17.bangladeshbank.Towhid.DataAnalyst;
 public class CleanandPreprocessDataController
 {
     @javafx.fxml.FXML
+    private TableColumn timeColumn;
+    @javafx.fxml.FXML
+    private Button applyButton;
+    @javafx.fxml.FXML
+    private TableColumn nameColumn;
+    @javafx.fxml.FXML
+    private TextArea notesArea;
+    @javafx.fxml.FXML
+    private CheckBox removeNullCheckBox;
+    @javafx.fxml.FXML
+    private ComboBox methodComboBox;
+    @javafx.fxml.FXML
+    private CheckBox normalizeCheckBox;
+    @javafx.fxml.FXML
+    private TableView datasetTable;
+    @javafx.fxml.FXML
+    private TableColumn typeColumn;
+    @javafx.fxml.FXML
+    private Button preprocessButton;
+    @javafx.fxml.FXML
+    private TextField outputField;
+
+    @javafx.fxml.FXML
     public void initialize() {
     }}
