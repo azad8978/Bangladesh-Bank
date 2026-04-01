c223e75a530d8717a03ca06f400297e3edb1de34
Towhid Azad <2312142@iub.edu.bd>
2026-04-02 05:43:04 +0600

    inital commit

---
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/CyberSecurityOfficer/CyberSecurityOfficerGenerateReportsController.java
@@ -0,0 +1,22 @@
+package com.spring26.section2.group17.bangladeshbank.Towhid.CyberSecurityOfficer;
+
+import javafx.scene.control.*;
+
+public class CyberSecurityOfficerGenerateReportsController
+{
+    @javafx.fxml.FXML
+    private TableView reportTable;
+    @javafx.fxml.FXML
+    private Button generateButton;
+    @javafx.fxml.FXML
+    private DatePicker endDate;
+    @javafx.fxml.FXML
+    private ComboBox reportTypeComboBox;
+    @javafx.fxml.FXML
+    private DatePicker startDate;
+    @javafx.fxml.FXML
+    private TextArea summaryArea;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
