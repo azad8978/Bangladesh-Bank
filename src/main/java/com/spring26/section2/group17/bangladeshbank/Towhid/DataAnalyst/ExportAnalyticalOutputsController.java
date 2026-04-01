c52facd909846658bb545136395cc4a430c833db
Towhid Azad <2312142@iub.edu.bd>
2026-04-02 04:26:59 +0600

    inital commit

---
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/DataAnalyst/ExportAnalyticalOutputsController.java
@@ -0,0 +1,22 @@
+package com.spring26.section2.group17.bangladeshbank.Towhid.DataAnalyst;
+
+import javafx.scene.control.*;
+
+public class ExportAnalyticalOutputsController
+{
+    @javafx.fxml.FXML
+    private TextArea statusArea;
+    @javafx.fxml.FXML
+    private Button exportButton;
+    @javafx.fxml.FXML
+    private ToggleGroup formatGroup;
+    @javafx.fxml.FXML
+    private RadioButton csvRadio;
+    @javafx.fxml.FXML
+    private ComboBox resultComboBox;
+    @javafx.fxml.FXML
+    private RadioButton pdfRadio;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
