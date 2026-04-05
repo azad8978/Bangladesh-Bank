69ecd6ee498e9f80d29895ee6d81d765ebfd77a1
M.H. Rafi <m.h.rafi436@gmail.com>
2026-04-05 23:39:06 +0600

    scenebuilder

--- src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/ForeignExchangeControlOfficer/MonitoringCrossBorderTradePaymentsController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/ForeignExchangeControlOfficer/MonitoringCrossBorderTradePaymentsController.java
@@ -1,7 +1,46 @@
 package com.spring26.section2.group17.bangladeshbank.MahmudulHasan.ForeignExchangeControlOfficer;
 
+import javafx.scene.control.*;
+
 public class MonitoringCrossBorderTradePaymentsController
 {
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TextField amountField;
+    @javafx.fxml.FXML
+    private TableColumn colImporter;
+    @javafx.fxml.FXML
+    private Button btnEnterPortal;
+    @javafx.fxml.FXML
+    private TableColumn colInvoice;
+    @javafx.fxml.FXML
+    private TableColumn colLC;
+    @javafx.fxml.FXML
+    private TextField exporterField;
+    @javafx.fxml.FXML
+    private TableView historyTable;
+    @javafx.fxml.FXML
+    private TableColumn colExporter;
+    @javafx.fxml.FXML
+    private Button btnVerify;
+    @javafx.fxml.FXML
+    private TextField importerField;
+    @javafx.fxml.FXML
+    private Label statusBar;
+    @javafx.fxml.FXML
+    private TextField invoiceField;
+    @javafx.fxml.FXML
+    private Button btnSubmit;
+    @javafx.fxml.FXML
+    private CheckBox invoiceCheck;
+    @javafx.fxml.FXML
+    private CheckBox shipmentCheck;
+    @javafx.fxml.FXML
+    private TextField lcField;
+    @javafx.fxml.FXML
+    private TableColumn colStatus;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
