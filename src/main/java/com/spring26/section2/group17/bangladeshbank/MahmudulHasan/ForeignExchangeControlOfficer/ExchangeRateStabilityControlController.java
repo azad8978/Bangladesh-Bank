77de5b0c8d73d94d44faca7752519995edcac3f4
M.H. Rafi <m.h.rafi436@gmail.com>
2026-04-05 23:17:26 +0600

    scenebuilder

--- src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/ForeignExchangeControlOfficer/ExchangeRateStabilityControlController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/ForeignExchangeControlOfficer/ExchangeRateStabilityControlController.java
@@ -1,7 +1,31 @@
 package com.spring26.section2.group17.bangladeshbank.MahmudulHasan.ForeignExchangeControlOfficer;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ExchangeRateStabilityControlController
 {
+
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @Deprecated
+    public void handleSave(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleValidate(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleReject(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleApprove(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/ForeignExchangeControlOfficer/RegulationofForeignCurrencyTransactionsController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/ForeignExchangeControlOfficer/RegulationofForeignCurrencyTransactionsController.java
@@ -1,7 +1,53 @@
 package com.spring26.section2.group17.bangladeshbank.MahmudulHasan.ForeignExchangeControlOfficer;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class RegulationofForeignCurrencyTransactionsController
 {
     @javafx.fxml.FXML
+    private TableColumn colReceiver;
+    @javafx.fxml.FXML
+    private TableColumn colAmount;
+    @javafx.fxml.FXML
+    private TableView transactionTable;
+    @javafx.fxml.FXML
+    private TextField receiverField;
+    @javafx.fxml.FXML
+    private TableColumn colPurpose;
+    @javafx.fxml.FXML
+    private TextField senderField;
+    @javafx.fxml.FXML
+    private TableColumn colSender;
+    @javafx.fxml.FXML
+    private TextField purposeField;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TextField amountField;
+    @javafx.fxml.FXML
+    private TableColumn colStatus;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleSave(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleValidate(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleReject(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleApprove(ActionEvent actionEvent) {
+    }
+}
