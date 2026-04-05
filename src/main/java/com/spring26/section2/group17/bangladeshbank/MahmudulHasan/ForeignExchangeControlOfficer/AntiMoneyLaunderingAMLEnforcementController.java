36a58eca8c6e6d2e9b05fff59f2f2b2af2b48f2e
M.H. Rafi <m.h.rafi436@gmail.com>
2026-04-06 01:16:05 +0600

    scenebuilder

--- src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/ForeignExchangeControlOfficer/AntiMoneyLaunderingAMLEnforcementController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/ForeignExchangeControlOfficer/AntiMoneyLaunderingAMLEnforcementController.java
@@ -1,7 +1,40 @@
 package com.spring26.section2.group17.bangladeshbank.MahmudulHasan.ForeignExchangeControlOfficer;
 
+import javafx.scene.control.*;
+
 public class AntiMoneyLaunderingAMLEnforcementController
 {
+    @javafx.fxml.FXML
+    private TableColumn colAmount;
+    @javafx.fxml.FXML
+    private TableView caseTable;
+    @javafx.fxml.FXML
+    private Button btnAnalyze;
+    @javafx.fxml.FXML
+    private TableColumn colRisk;
+    @javafx.fxml.FXML
+    private TextField accountField;
+    @javafx.fxml.FXML
+    private Label alertLabel;
+    @javafx.fxml.FXML
+    private TextField transactionField;
+    @javafx.fxml.FXML
+    private TextField countryField;
+    @javafx.fxml.FXML
+    private TextField amountField;
+    @javafx.fxml.FXML
+    private TableColumn colAccount;
+    @javafx.fxml.FXML
+    private Label riskScoreLabel;
+    @javafx.fxml.FXML
+    private Button btnSubmit;
+    @javafx.fxml.FXML
+    private Button btnStart;
+    @javafx.fxml.FXML
+    private ProgressIndicator riskIndicator;
+    @javafx.fxml.FXML
+    private TableColumn colTxn;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
