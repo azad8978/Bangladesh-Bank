1204f676b0fc0fe130ceb9d96e5ff9fdb72f3d07
M.H. Rafi <m.h.rafi436@gmail.com>
2026-04-05 23:44:26 +0600

    scenebuilder

--- src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/ForeignExchangeControlOfficer/ExchangeRateStabilityControlController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/ForeignExchangeControlOfficer/ExchangeRateStabilityControlController.java
@@ -1,13 +1,49 @@
 package com.spring26.section2.group17.bangladeshbank.MahmudulHasan.ForeignExchangeControlOfficer;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class ExchangeRateStabilityControlController
 {
+
+    @javafx.fxml.FXML
+    private Button btnSubmitFX;
+    @javafx.fxml.FXML
+    private Button btnAnalyze;
+    @javafx.fxml.FXML
+    private TextField buyRateField;
+    @javafx.fxml.FXML
+    private Label volatilityLabel;
+    @javafx.fxml.FXML
+    private TableColumn colVolatility;
+    @javafx.fxml.FXML
+    private Button btnOpenModule;
+    @javafx.fxml.FXML
+    private DatePicker datePicker;
+    @javafx.fxml.FXML
+    private Label deviationLabel;
+    @javafx.fxml.FXML
+    private TextField bankField;
+    @javafx.fxml.FXML
+    private ComboBox currencyBox;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private ProgressBar stabilityBar;
+    @javafx.fxml.FXML
+    private TableView historyTable;
+    @javafx.fxml.FXML
+    private TextField sellRateField;
+    @javafx.fxml.FXML
+    private TableColumn colBank;
+    @javafx.fxml.FXML
+    private TableColumn colBuy;
+    @javafx.fxml.FXML
+    private Label statusBar;
+    @javafx.fxml.FXML
+    private TableColumn colSell;
+    @javafx.fxml.FXML
+    private TableColumn colCurrency;
 
     @javafx.fxml.FXML
     public void initialize() {
