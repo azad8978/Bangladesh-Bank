1702f63946d0211fabd4cd57630b59fe822d19f5
M.H. Rafi <m.h.rafi436@gmail.com>
2026-04-07 21:28:08 +0600

    initial commit

--- src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/PaymentSystemsOfficer/MonitorFraudRiskController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/PaymentSystemsOfficer/MonitorFraudRiskController.java
@@ -1,7 +1,56 @@
 package com.spring26.section2.group17.bangladeshbank.MahmudulHasan.PaymentSystemsOfficer;
 
+import javafx.scene.control.*;
+
 public class MonitorFraudRiskController
 {
+    @javafx.fxml.FXML
+    private Label statusProcess;
+    @javafx.fxml.FXML
+    private Label statusOutput;
+    @javafx.fxml.FXML
+    private Button processBtn;
+    @javafx.fxml.FXML
+    private TableColumn amountCol;
+    @javafx.fxml.FXML
+    private TextField accountField;
+    @javafx.fxml.FXML
+    private TextField txnIdField;
+    @javafx.fxml.FXML
+    private TableColumn resultCol;
+    @javafx.fxml.FXML
+    private TextField dateField;
+    @javafx.fxml.FXML
+    private Button verifyBtn;
+    @javafx.fxml.FXML
+    private Label statusValidate;
+    @javafx.fxml.FXML
+    private Label statusVerify;
+    @javafx.fxml.FXML
+    private TableView settlementTable;
+    @javafx.fxml.FXML
+    private TableColumn accountCol;
+    @javafx.fxml.FXML
+    private TextField amountField;
+    @javafx.fxml.FXML
+    private Label statusInput;
+    @javafx.fxml.FXML
+    private Label statusAccess;
+    @javafx.fxml.FXML
+    private TableColumn dateCol;
+    @javafx.fxml.FXML
+    private TableColumn statusCol;
+    @javafx.fxml.FXML
+    private Button accessBtn;
+    @javafx.fxml.FXML
+    private Button outputBtn;
+    @javafx.fxml.FXML
+    private TableColumn txnCol;
+    @javafx.fxml.FXML
+    private Button validateBtn;
+    @javafx.fxml.FXML
+    private Button inputBtn;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
