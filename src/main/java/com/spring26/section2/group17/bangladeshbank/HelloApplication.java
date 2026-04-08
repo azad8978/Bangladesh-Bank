ed1ef2697f41c046ad15e4d33bca1c7f121324e5
MH-Rafi-436 <m.h.rafi436@gmail.com>
2026-04-08 14:40:22 +0600

    initial commit

--- src/main/java/com/spring26/section2/group17/bangladeshbank/HelloApplication.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/PaymentSystemsOfficer/MonitorFraud&Risk.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
