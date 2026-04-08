c07e224a74cc9e28d8ee6b09b0729f009a317b78
M.H. Rafi <m.h.rafi436@gmail.com>
2026-04-08 10:34:31 +0600

    initial commit

--- src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/PaymentSystemsOfficer/ManageElectronicFundTransfersController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/PaymentSystemsOfficer/ManageElectronicFundTransfersController.java
@@ -7,36 +7,6 @@ import javafx.scene.control.TextField;
 
 public class ManageElectronicFundTransfersController
 {
-    @javafx.fxml.FXML
-    private TableView transactionTable;
-    @javafx.fxml.FXML
-    private Button processBtn;
-    @javafx.fxml.FXML
-    private TableColumn amountCol;
-    @javafx.fxml.FXML
-    private TextField accountField;
-    @javafx.fxml.FXML
-    private TableColumn resultCol;
-    @javafx.fxml.FXML
-    private Button verifyBtn;
-    @javafx.fxml.FXML
-    private TableColumn accountCol;
-    @javafx.fxml.FXML
-    private TextField amountField;
-    @javafx.fxml.FXML
-    private TextField referenceField;
-    @javafx.fxml.FXML
-    private TableColumn statusCol;
-    @javafx.fxml.FXML
-    private Button accessBtn;
-    @javafx.fxml.FXML
-    private Button outputBtn;
-    @javafx.fxml.FXML
-    private Button validateBtn;
-    @javafx.fxml.FXML
-    private Button inputBtn;
-    @javafx.fxml.FXML
-    private TableColumn referenceCol;
 
     @javafx.fxml.FXML
     public void initialize() {
