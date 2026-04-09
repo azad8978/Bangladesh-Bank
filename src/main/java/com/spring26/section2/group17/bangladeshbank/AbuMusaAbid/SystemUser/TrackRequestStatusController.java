c8e0c64bf06e678ed35f97c82715fda5e5a6bed6
abumusaabid <2310141@iub.edu.bd>
2026-04-10 00:35:12 +0600

    FXML Design

--- src/main/java/com/spring26/section2/group17/bangladeshbank/AbuMusaAbid/SystemUser/TrackRequestStatusController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/AbuMusaAbid/SystemUser/TrackRequestStatusController.java
@@ -1,7 +1,24 @@
 package com.spring26.section2.group17.bangladeshbank.AbuMusaAbid.SystemUser;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
 public class TrackRequestStatusController
 {
+    @javafx.fxml.FXML
+    private TableView requestTable;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TableColumn dateColumn;
+    @javafx.fxml.FXML
+    private TableColumn typeColumn;
+    @javafx.fxml.FXML
+    private TextArea timelineArea;
+    @javafx.fxml.FXML
+    private TableColumn idColumn;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
