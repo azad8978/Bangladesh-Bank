3e74f1de35bc333c906600a08bfd4746001f18a5
M.H. Rafi <m.h.rafi436@gmail.com>
2026-04-12 01:32:02 +0600

    Deshbord Controller Class Edit with Controller Class

--- src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/PaymentSystemsOfficer/DeshbordController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/MahmudulHasan/PaymentSystemsOfficer/DeshbordController.java
@@ -1,47 +1,101 @@
 package com.spring26.section2.group17.bangladeshbank.MahmudulHasan.PaymentSystemsOfficer;
 
+import com.spring26.section2.group17.bangladeshbank.SceneSwitcher;
 import javafx.event.ActionEvent;
 
-public class DeshbordController
-{
+public class DeshbordController {
     @javafx.fxml.FXML
     public void initialize() {
     }
 
     @javafx.fxml.FXML
-    public void HandleDisputeResolution(ActionEvent actionEvent) {
+    public void ManageElectronicFundTransfers(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("MahmudulHasan/PaymentSystemsOfficer/RegulationofForeignCurrencyTransactions.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void MonitorTransactionSettlement(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void MonitorFraudRisk(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void ManageElectronicFundTransfers(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void GeneratePaymentPerformanceReports(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void ManageBankPSPIntegration(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("MahmudulHasan/PaymentSystemsOfficer/MonitoringCross-BorderTradePaymentsController.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void OverseePaymentGatewayOperations(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("MahmudulHasan/PaymentSystemsOfficer/ExchangeRateStabilityControl.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Deprecated
+    public void EnsureRegulatoryCompliance(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("MahmudulHasan/PaymentSystemsOfficer/Anti-MoneyLaundering(AML)Enforcement.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
-    public void EnsureRegulatoryCompliance(ActionEvent actionEvent) {
+    public void HandleDisputeResolution(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("MahmudulHasan/PaymentSystemsOfficer/SupervisionofAuthorizedDealer(AD)Banks.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Deprecated
+    public void Button(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("Towhid/Dashboard.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Deprecated
+    public void MonitorFraudRisk(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("MahmudulHasan/PaymentSystemsOfficer/FXPolicyImplementation.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void ManageBankPSPIntegration(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("MahmudulHasan/PaymentSystemsOfficer/RemittanceFlowRegulation.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void GeneratePaymentPerformanceReports(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("MahmudulHasan/PaymentSystemsOfficer/FXDataIntelligence&Reporting.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
     public void BackButton(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("Towhid/Dashboard.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
