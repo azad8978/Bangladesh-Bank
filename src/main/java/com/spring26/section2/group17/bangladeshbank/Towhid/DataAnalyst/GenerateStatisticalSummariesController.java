cf7d0c97bd2941ca4db114533aacb1d1d3a3237f
Towhid Azad <2312142@iub.edu.bd>
2026-04-12 00:37:17 +0600

    inital commit

--- src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/DataAnalyst/GenerateStatisticalSummariesController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/DataAnalyst/GenerateStatisticalSummariesController.java
@@ -2,43 +2,149 @@ package com.spring26.section2.group17.bangladeshbank.Towhid.DataAnalyst;
 
 import com.spring26.section2.group17.bangladeshbank.SceneSwitcher;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
-public class GenerateStatisticalSummariesController
-{
-    @javafx.fxml.FXML
-    private ComboBox dataTypeComboBox;
-    @javafx.fxml.FXML
-    private Button generateButton;
-    @javafx.fxml.FXML
-    private CheckBox meanCheckBox;
-    @javafx.fxml.FXML
-    private CheckBox modeCheckBox;
-    @javafx.fxml.FXML
-    private TextArea inputDataArea;
-    @javafx.fxml.FXML
-    private CheckBox stdDevCheckBox;
-    @javafx.fxml.FXML
-    private TextArea resultArea;
-    @javafx.fxml.FXML
-    private Button statsButton;
-    @javafx.fxml.FXML
-    private CheckBox medianCheckBox;
+import java.util.*;
 
-    @javafx.fxml.FXML
+public class GenerateStatisticalSummariesController {
+
+    @FXML private ComboBox<String> dataTypeComboBox;
+    @FXML private Button generateButton;
+    @FXML private CheckBox meanCheckBox;
+    @FXML private CheckBox modeCheckBox;
+    @FXML private TextArea inputDataArea;
+    @FXML private CheckBox stdDevCheckBox;
+    @FXML private TextArea resultArea;
+    @FXML private Button statsButton;
+    @FXML private CheckBox medianCheckBox;
+
+    private ArrayList<Double> dataList = new ArrayList<>();
+
+    @FXML
     public void initialize() {
+
+        dataTypeComboBox.getItems().addAll("Security Logs", "Transactions", "User Activity");
+
+
+        inputDataArea.setText("10,20,30,40,50,20,30");
+
+
+        statsButton.setOnAction(this::handleStatistics);
     }
 
-    @javafx.fxml.FXML
-    public void ButtonOA(ActionEvent actionEvent) {
 
-        try{
-            SceneSwitcher.switchTo("Towhid/DataAnalyst/DashboardDA.fxml");
+    @FXML
+    public void handleStatistics(ActionEvent event) {
+
+        dataList.clear();
+
+        try {
+            String input = inputDataArea.getText();
+
+            if (input.isEmpty()) {
+                resultArea.setText("⚠ Please enter data!");
+                return;
+            }
+
+
+            String[] numbers = input.split(",");
+
+            for (String num : numbers) {
+                dataList.add(Double.parseDouble(num.trim()));
+            }
+
+            StringBuilder result = new StringBuilder();
+
+
+            if (!meanCheckBox.isSelected() &&
+                    !medianCheckBox.isSelected() &&
+                    !modeCheckBox.isSelected() &&
+                    !stdDevCheckBox.isSelected()) {
+
+                resultArea.setText("⚠ Select at least one metric!");
+                return;
+            }
+
+
+            if (meanCheckBox.isSelected()) {
+                result.append("Mean: ").append(String.format("%.2f", calculateMean())).append("\n");
+            }
+
+            if (medianCheckBox.isSelected()) {
+                result.append("Median: ").append(calculateMedian()).append("\n");
+            }
+
+            if (modeCheckBox.isSelected()) {
+                result.append("Mode: ").append(calculateMode()).append("\n");
+            }
+
+            if (stdDevCheckBox.isSelected()) {
+                result.append("Std Dev: ").append(String.format("%.2f", calculateStdDev())).append("\n");
+            }
+
+            resultArea.setText(result.toString());
+
+        } catch (Exception e) {
+            resultArea.setText("❌ Invalid input! Use format: 10,20,30");
         }
-        catch (Exception e){
+    }
+
+
+    private double calculateMean() {
+        double sum = 0;
+        for (double num : dataList) sum += num;
+        return sum / dataList.size();
+    }
+
+
+    private double calculateMedian() {
+        Collections.sort(dataList);
+        int size = dataList.size();
+
+        if (size % 2 == 0) {
+            return (dataList.get(size/2 - 1) + dataList.get(size/2)) / 2;
+        }
+        return dataList.get(size/2);
+    }
+
+
+    private double calculateMode() {
+        Map<Double, Integer> map = new HashMap<>();
+
+        for (double num : dataList) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        double mode = 0;
+        int max = 0;
+
+        for (Map.Entry<Double, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                mode = entry.getKey();
+            }
+        }
+        return mode;
+    }
+
+    private double calculateStdDev() {
+        double mean = calculateMean();
+        double sum = 0;
+
+        for (double num : dataList) {
+            sum += Math.pow(num - mean, 2);
+        }
+
+        return Math.sqrt(sum / dataList.size());
+    }
+
+
+    @FXML
+    public void ButtonOA(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("Towhid/DataAnalyst/DashboardDA.fxml");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/DataAnalyst/VisualizeAnalyticalDataController.java
+++ src/main/java/com/spring26/section2/group17/bangladeshbank/Towhid/DataAnalyst/VisualizeAnalyticalDataController.java
@@ -2,42 +2,145 @@ package com.spring26.section2.group17.bangladeshbank.Towhid.DataAnalyst;
 
 import com.spring26.section2.group17.bangladeshbank.SceneSwitcher;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.fxml.FXML;
+import javafx.scene.chart.*;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 
-public class VisualizeAnalyticalDataController
-{
-    @javafx.fxml.FXML
-    private Button visualizeButton;
-    @javafx.fxml.FXML
-    private RadioButton barChartRadio;
-    @javafx.fxml.FXML
-    private RadioButton pieChartRadio;
-    @javafx.fxml.FXML
-    private ComboBox datasetComboBox;
-    @javafx.fxml.FXML
-    private Pane chartPane;
-    @javafx.fxml.FXML
-    private Button renderButton;
-    @javafx.fxml.FXML
-    private RadioButton lineChartRadio;
-    @javafx.fxml.FXML
-    private ToggleGroup chartGroup;
+import java.util.ArrayList;
 
-    @javafx.fxml.FXML
+public class VisualizeAnalyticalDataController {
+
+    @FXML private RadioButton barChartRadio;
+    @FXML private RadioButton lineChartRadio;
+    @FXML private RadioButton pieChartRadio;
+    @FXML private ToggleGroup chartGroup;
+
+    @FXML private ComboBox<String> datasetComboBox;
+    @FXML private Pane chartPane;
+    @FXML private Button renderButton;
+
+
+    private ArrayList<String> labels = new ArrayList<>();
+    private ArrayList<Integer> values = new ArrayList<>();
+
+    @FXML
     public void initialize() {
+
+
+        barChartRadio.setToggleGroup(chartGroup);
+        lineChartRadio.setToggleGroup(chartGroup);
+        pieChartRadio.setToggleGroup(chartGroup);
+
+
+        datasetComboBox.getItems().addAll("Monthly Data");
+        datasetComboBox.getSelectionModel().selectFirst();
+
+
+        labels.add("Jan");
+        labels.add("Feb");
+        labels.add("Mar");
+        labels.add("Apr");
+
+        values.add(20);
+        values.add(50);
+        values.add(30);
+        values.add(80);
+
+
+        chartPane.setPrefSize(600, 400);
+
+
+        renderButton.setOnAction(this::handleRender);
     }
 
-    @javafx.fxml.FXML
-    public void ButtonOA(ActionEvent actionEvent) {
 
-        try{
-            SceneSwitcher.switchTo("Towhid/DataAnalyst/DashboardDA.fxml");
+    @FXML
+    public void handleRender(ActionEvent event) {
+
+        chartPane.getChildren().clear();
+
+        if (barChartRadio.isSelected()) {
+            showBarChart();
         }
-        catch (Exception e){
+        else if (lineChartRadio.isSelected()) {
+            showLineChart();
+        }
+        else if (pieChartRadio.isSelected()) {
+            showPieChart();
+        }
+        else {
+            chartPane.getChildren().add(new Label("⚠ Please select a chart type!"));
+        }
+    }
+
+
+    private void showBarChart() {
+
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
+        barChart.setTitle("Bar Chart");
+
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+
+        for (int i = 0; i < labels.size(); i++) {
+            series.getData().add(new XYChart.Data<>(labels.get(i), values.get(i)));
+        }
+
+        barChart.getData().add(series);
+
+
+        barChart.setPrefSize(chartPane.getWidth(), chartPane.getHeight());
+
+        chartPane.getChildren().add(barChart);
+    }
+
+
+    private void showLineChart() {
+
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Line Chart");
+
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+
+        for (int i = 0; i < labels.size(); i++) {
+            series.getData().add(new XYChart.Data<>(labels.get(i), values.get(i)));
+        }
+
+        lineChart.getData().add(series);
+
+        lineChart.setPrefSize(chartPane.getWidth(), chartPane.getHeight());
+
+        chartPane.getChildren().add(lineChart);
+    }
+
+
+    private void showPieChart() {
+
+        PieChart pieChart = new PieChart();
+
+        for (int i = 0; i < labels.size(); i++) {
+            pieChart.getData().add(new PieChart.Data(labels.get(i), values.get(i)));
+        }
+
+        pieChart.setTitle("Pie Chart");
+
+        pieChart.setPrefSize(chartPane.getWidth(), chartPane.getHeight());
+
+        chartPane.getChildren().add(pieChart);
+    }
+
+
+    @FXML
+    public void ButtonOA(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("Towhid/DataAnalyst/DashboardDA.fxml");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
